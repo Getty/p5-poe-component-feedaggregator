9af6ca84cd229f385f21e0b194589d6a577bf3fb
Torsten Raudssus <github@raudssus.de>
2011-02-15 06:38:53 +0100

    Added logger docking capability

--- lib/POE/Component/FeedAggregator.pm
+++ lib/POE/Component/FeedAggregator.pm
@@ -11,14 +11,22 @@ use IO::All;
 
 our $VERSION ||= '0.0development';
 
+has logger => (
+	is => 'rw',
+	predicate => 'has_logger',
+);
+
 has feed_client => (
 	isa => 'POE::Component::Client::Feed',
 	is => 'ro',
 	lazy => 1,
 	required => 1,
 	default => sub {
+		my $self = shift;
+		$self->logger->info($self->logger_prefix.'Starting up POE::Component::Client::Feed') if $self->has_logger;
 		POE::Component::Client::Feed->new({
 			http_agent => __PACKAGE__.'/'.$VERSION,
+			logger   => $self->logger,
 		});
 	},
 );
@@ -32,8 +40,10 @@ has tmpdir => (
 
 event feed_received => sub {
 	my ( $self, $kernel, @args ) = @_[ OBJECT, KERNEL, ARG0..$#_ ];
+	$self->logger->debug($self->logger_prefix.'Feed received') if $self->has_logger;
 	my $http_request = $args[0];
 	my $feed = $args[2];
+	$self->logger->debug($self->logger_prefix.'Request feed '.$feed->url.' delayed with '.$feed->delay.' seconds') if $self->has_logger;
 	$kernel->delay( 'request_feed', $feed->delay, $feed );
 	my $xml_feed = $args[1];
 	return if !(ref $xml_feed);
@@ -46,6 +56,7 @@ event feed_received => sub {
 		$ignore = $feed->ignore_first;
 	}
 	my @new_entries;
+	$self->logger->debug($self->logger_prefix.( scalar $xml_feed->entries ).' Entries found') if $self->has_logger;
 	for my $entry ($xml_feed->entries) {
 		my $link = $entry->link;
 		my $title = $entry->title;
@@ -64,14 +75,17 @@ event feed_received => sub {
 		push @new_entries, $link.' '.$title;
 		$kernel->post( $feed->sender, $feed->entry_event, $feed, $entry ) if (!$known and !$ignore);
 	}
+	$self->logger->debug($self->logger_prefix.( scalar @new_entries ).' new entries') if $self->has_logger;
 	push @entries, @new_entries;
 	my $count = @entries;
 	my @save_entries = splice(@entries, $count - $feed->max_headlines > 0 ? $count - $feed->max_headlines : 0, $feed->max_headlines);
+	$self->logger->debug($self->logger_prefix.'Saving cache file '.$cache_file) if $self->has_logger;
 	scalar join("\n",@save_entries) > io($cache_file);
 };
 
 event request_feed => sub {
 	my ( $self, $feed ) = @_[ OBJECT, ARG0..$#_ ];
+	$self->logger->debug($self->logger_prefix.'Requesting feed '.$feed->url) if $self->has_logger;
 	$self->feed_client->yield('request',$feed->url,'feed_received',$feed);
 };
 
@@ -81,8 +95,14 @@ event _add_feed => sub {
 	my ( $self, $sender, $feed_args ) = @_[ OBJECT, SENDER, ARG0..$#_ ];
 	$feed_args->{sender} = $sender;
 	my $feed = POE::Component::FeedAggregator::Feed->new($feed_args);
+	$self->logger->debug($self->logger_prefix.'Added feed '.$feed->url) if $self->has_logger;
 	$self->yield( request_feed => $feed );
 };
+
+sub logger_prefix {
+	my $self = shift;
+	__PACKAGE__.' ('.$self->get_session_id.') ';
+}
 
 1;
 
